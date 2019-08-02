05ad2c276abc59fde30f4307cfab51cfb18cd2a7
SEBUJAS <SEBUJAS@SEBU>
2019-08-02 13:02:09 +0530

    1

---
+++ CCTransactionBankApproval/src/main/java/com/itc/trn/mis/CcTransactionBankApprovalApplication.java
@@ -0,0 +1,13 @@
+package com.itc.trn.mis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcTransactionBankApprovalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcTransactionBankApprovalApplication.class, args);
+	}
+
+}
