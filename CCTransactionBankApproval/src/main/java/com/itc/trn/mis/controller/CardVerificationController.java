5b708e3db08018f3774e050a1f9baf80cf0088fe
34807 <34807@BLRATIFWN34807.ITCINFOTECH.com>
2019-08-05 20:24:50 +0530

    3

--- CCTransactionBankApproval/src/main/java/com/itc/trn/mis/controller/CardVerificationController.java
+++ CCTransactionBankApproval/src/main/java/com/itc/trn/mis/controller/CardVerificationController.java
@@ -1,14 +1,12 @@
 package com.itc.trn.mis.controller;
 
-import javax.websocket.server.PathParam;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/cardverification")
@@ -17,7 +15,7 @@ public class CardVerificationController {
 	private static final Logger logger = LoggerFactory.getLogger(CardVerificationController.class);
 
 	@GetMapping("/transactionapproval/{cardNo}")
-	public String verifyCard(@PathParam("cardNo") String cardNo) {
+	public String verifyCard(@PathVariable("cardNo") String cardNo) {
 		logger.info("CardVerificationController-PayLoad::"+cardNo);
 		//
 		return "APR";
